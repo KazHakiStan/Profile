76bf38df6b56c3869bd1c479f276b6fdfef2363e
Danila Zakharchuk <zakharchukdanila@gmail.com>
2024-11-26 00:57:10 +0100

    first version

---
+++ index.js
@@ -0,0 +1,10 @@
+const linkedin = document.getElementById("in");
+const github = document.getElementById("git");
+
+linkedin.addEventListener("click", () => {
+    window.location.href = 'https://www.linkedin.com/in/danila-zakharchuk-1aa11b292/'
+});
+
+github.addEventListener("click", () => {
+    window.location.href = 'https://github.com/KazHakiStan'
+});
